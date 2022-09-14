5ae712b0fd6a7e966211b0ab469bf32df79a7cdd
Renan <renan23eduardo@gmail.com>
2022-09-14 15:06:04 -0300

    Create background component

--- App.tsx
+++ App.tsx
@@ -1,53 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import { StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
+import { Background } from './src/Components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Olá Renan </Text>
-      <Text style={styles.title}>R</Text>
-      <Button title='Sending' />
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar barStyle="light-content" backgroundColor='transparent' translucent />
+    </Background>
   );
 }
 
-interface ButtonProps{
-  title: string,
-  color: string
-}
 
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity style={estilo.botao}>
-      <Text style={estilo.texto}>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
 
-const estilo = StyleSheet.create({
-  texto:{
-    color: '#fff',
-    fontSize: 20,
-  },
-  botao:{
-    backgroundColor: 'green',
-    padding: 10,
-    borderRadius: 10,
-  }
-})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f8f8f8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize:44,
-    color: '#000'
-  }
-});
