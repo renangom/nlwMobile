08932d644c718a9ef17c7145644e4eb166a1d32e
Renan <renan23eduardo@gmail.com>
2022-09-19 13:32:24 -0300

    Make api request

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,14 +2,23 @@ import { FlatList, Image, Text, View } from "react-native";
 import { styles } from "./styles";
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from "../../Components/Heading";
-import { GameCard } from "../../Components/GameCard";
-import { GAMES } from "../../utils/games";
+import { GameCard, GameCardProps } from "../../Components/GameCard";
+import { useEffect, useState } from "react";
+
+
+
 export function Home(){
+    const [games, setGames] = useState<GameCardProps[]>([])
+    useEffect(() => {
+        fetch('http://10.0.0.164:3333/games')
+        .then(response => response.json())
+        .then(data => setGames(data))
+    }, [])
     return(
         <View style={styles.container}> 
             <Image source={logoImg} style={styles.logo} />
             <Heading title="Encontre seu duo" subtitle="Selecione o game que deseja jogar..." />
-            <FlatList contentContainerStyle={styles.contentList} horizontal data={GAMES} keyExtractor={item => item.id} renderItem={({item}) => (<GameCard data={item} />)} />
+            <FlatList contentContainerStyle={styles.contentList} horizontal data={games} keyExtractor={item => item.id} renderItem={({item}) => (<GameCard data={item} />)} />
             
         </View>
     )
